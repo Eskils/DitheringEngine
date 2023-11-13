b0e75dc7b2f98d64903a83ac32e48f59bdf850d0
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2023-11-13 13:40:57 +0100

    Remove wrappedType from SettingsConfiguration

--- Sources/DitheringEngine/SettingsConfiguration.swift
+++ Sources/DitheringEngine/SettingsConfiguration.swift
@@ -10,12 +10,9 @@ import Combine
 
 public protocol SettingsConfiguration: AnyObject, Codable {
     func didChange() -> AnyPublisher<Any, Never>
-    var wrappedType: SettingsConfiguration.Type { get }
     var className: String { get }
 }
 
 extension SettingsConfiguration {
-    public var wrappedType: SettingsConfiguration.Type { Self.self }
-    
     public var className: String { NSStringFromClass(Self.self) }
 }
