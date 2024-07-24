3d0d9cfef4c95d804583a3d519c217e78e981260
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2024-07-24 20:09:52 +0200

    Add support for intensity with White Noise dithering

--- Sources/DitheringEngine/DitherMethods/Dither-WhiteNoise.swift
+++ Sources/DitheringEngine/DitherMethods/Dither-WhiteNoise.swift
@@ -6,11 +6,11 @@
 //
 
 extension DitherMethods {
-    func whiteNoise(palette: BytePalette, thresholdMapSize: Int, performOnCPU: Bool) {
+    func whiteNoise(palette: BytePalette, thresholdMapSize: Int, intensity: Float, performOnCPU: Bool) {
         let thresholdMapSize = clamp(thresholdMapSize, min: 2, max: 256)
         let thresholdMap = FloatingThresholdMap.generateWhiteNoiseThresholdMap(n: thresholdMapSize, max: 255, seed: seed)
         let normalizationOffset: Float = 128
-        let thresholdMultiplier: Float = 1.0
+        let thresholdMultiplier: Float = intensity
         
         ordered(
             palette: palette,
