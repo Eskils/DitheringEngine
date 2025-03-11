24fd8eab42a45af4f61258096268a89b2464f61f
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-03-11 20:00:29 +0100

    Rewrite UIImage extensions to use CoreGraphics APIs only

--- Documentation/Demo/Dithering/Extensions/CGImage+Extension.swift
+++ Documentation/Demo/Dithering/Extensions/CGImage+Extension.swift
@@ -5,14 +5,32 @@
 //  Created by Eskil Gjerde Sviggum on 11/03/2025.
 //
 
+import CoreGraphics
+import CoreImage
+#if canImport(UIKit)
 import UIKit
+#endif
 
 extension CGImage {
+    #if canImport(UIKit)
     func toUIImage() -> UIImage {
         UIImage(cgImage: self)
     }
+    #endif
     
     var size: CGSize {
         CGSize(width: width, height: height)
+    }
+    
+    func blur(radius: CGFloat) -> CGImage? {
+        let context = CIContext()
+        let input = CIImage(cgImage: self)
+        let filter = CIFilter(name: "CIGaussianBlur")!
+        filter.setValue(input, forKey: kCIInputImageKey)
+        filter.setValue(radius, forKey: "inputRadius")
+        guard let result = filter.value(forKey: kCIOutputImageKey) as? CIImage,
+              let resCG = context.createCGImage(result, from: input.extent)
+        else { return nil }
+        return resCG
     }
 }
