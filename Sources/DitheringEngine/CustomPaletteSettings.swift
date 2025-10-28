9e6a67b362bc2e91968e8965d200af96dbca7704
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-10-28 07:59:22 +0100

    Add protocol CustomPaletteSettings and implement conformance in CustomPaletteSettingsConfiguration

---
+++ Sources/DitheringEngine/CustomPaletteSettings.swift
@@ -0,0 +1,10 @@
+//
+//  CustomPaletteSettings.swift
+//  DitheringEngine
+//
+//  Created by Eskil Gjerde Sviggum on 28/10/2025.
+//
+
+public protocol CustomPaletteSettings: SettingsConfiguration {
+    func palette(imageDescription: ImageDescription?, preferNoGray: Bool) -> BytePalette
+}
