578caff2ffc8022e536aed205fe462fbe055d144
Eskil Gjerde Sviggum <eskilsviggum@gmail.com>
2025-10-31 08:56:33 +0100

    Add model ImageDescriptionFormat

---
+++ Sources/DitheringEngine/ImageDescriptionFormat.swift
@@ -0,0 +1,11 @@
+//
+//  ImageDescriptionFormat.swift
+//  DitheringEngine
+//
+//  Created by Eskil Gjerde Sviggum on 31/10/2025.
+//
+
+public struct ImageDescriptionFormat {
+    public let byte: ImageDescription
+    public let float: FloatingImageDescription
+}
